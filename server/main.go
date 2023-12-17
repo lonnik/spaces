726e210f763843ab37fa69c12f280d06735732f0
Nikolas <niko.heidner@gmail.com>
2023-12-17 17:15:44 +0100

    feat: use docker in server

--- server/main.go
+++ server/main.go
@@ -26,6 +26,6 @@ func main() {
 		http.Redirect(w, r, customSchemeURI, http.StatusFound)
 	})
 
-	log.Println("Starting server on :80")
-	log.Fatal(http.ListenAndServe(":80", m))
+	log.Println("Starting server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", m))
 }
