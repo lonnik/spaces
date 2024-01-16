98ccf2027bebf633887f884309885dace5ecfe5b
Nikolas <niko.heidner@gmail.com>
2024-01-16 12:56:58 +0100

    feat: add basic styling to space items on here screen - using flatlist and refresh by swiping up

--- frontend/src/screens/Here.tsx
+++ frontend/src/screens/Here.tsx
@@ -1,11 +1,10 @@
 import "react-native-gesture-handler";
-import { Button, Text, View } from "react-native";
+import { Button, Text, View, ActivityIndicator, FlatList } from "react-native";
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
-import { FC, useEffect, useState } from "react";
-import { Location, TabsParamList } from "../types";
+import { FC, useCallback, useEffect, useState } from "react";
+import { Location, Space, TabsParamList } from "../types";
 import { useQuery } from "@tanstack/react-query";
 import {
-  LocationObject,
   requestForegroundPermissionsAsync,
   getCurrentPositionAsync,
 } from "expo-location";
@@ -14,7 +13,8 @@ import { getSpacesByLocation } from "../utils/queries";
 export const HereScreen: FC<BottomTabScreenProps<TabsParamList, "Here">> = ({
   navigation,
 }) => {
-  const [location, setLocation] = useState<LocationObject | null>(null);
+  const [location, setLocation] = useState<Location | null>(null);
+  const [refreshing, setRefreshing] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -25,30 +25,78 @@ export const HereScreen: FC<BottomTabScreenProps<TabsParamList, "Here">> = ({
       }
 
       const location = await getCurrentPositionAsync({});
-      setLocation(location);
+      setLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      });
     })();
   }, []);
 
-  let coords = location
-    ? {
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-      }
-    : null;
-
-  const { data: spaces } = useQuery({
+  const {
+    data: spaces,
+    isLoading,
+    refetch,
+  } = useQuery({
     queryKey: ["spaces by location"],
-    queryFn: () => getSpacesByLocation(coords as Location),
-    enabled: !!coords,
+    queryFn: () => getSpacesByLocation(location as Location),
+    enabled: !!location,
   });
 
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <Button
         title="Profile"
         onPress={() => navigation.navigate("Profile" as any)}
       />
-      <Text>{JSON.stringify(spaces)}</Text>
+      <FlatList
+        data={spaces}
+        numColumns={3}
+        onRefresh={onRefresh}
+        refreshing={refreshing}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => {
+          return <SpaceItem data={item} />;
+        }}
+        style={{ flex: 1, padding: 5 }}
+      />
+    </View>
+  );
+};
+
+const SpaceItem: FC<{ data: Space }> = ({ data }) => {
+  return (
+    <View
+      style={{
+        width: "33.33333%",
+        padding: 5,
+        aspectRatio: 1,
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: `#${data.themeColorHexaCode}`,
+          borderRadius: 7,
+          marginVertical: 0,
+          paddingHorizontal: 0,
+        }}
+      >
+        <Text>{JSON.stringify(data)}</Text>
+      </View>
     </View>
   );
 };
